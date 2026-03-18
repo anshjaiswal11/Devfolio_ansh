8c412369c4310674b1ed99a5952743fc7e72744a
Ansh Jaiswal <145288803+anshjaiswal11@users.noreply.github.com>
2026-03-19 00:05:29 +0530

    new-changes

--- fixBlogs.js
+++ fixBlogs.js
@@ -19,3 +19,24 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/portfolio')
   console.log('Fixed', blogs.length, 'blogs')
   process.exit(0)
 }).catch(console.error)
+
+
+const fixSlugs = async () => {
+  await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/portfolio')
+  const blogs = await Blog.find()
+  for (let blog of blogs) {
+    if (blog.slug && blog.slug.includes('http')) {
+      blog.slug = blog.title
+        .toLowerCase()
+        .replace(/[^a-z0-9\s-]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+/g, '-')
+        .trim()
+    }
+    await blog.save()
+  }
+  console.log('Fixed', blogs.length, 'blogs')
+  process.exit(0)
+}
+
+fixSlugs()
